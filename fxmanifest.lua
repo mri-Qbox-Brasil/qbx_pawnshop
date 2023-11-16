72405bfe5716a6dc1752c13e9dfaa91693eab07d
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-11-16 13:50:44 -0500

    fix: remove import.lua
    * fix: remove import.lua
    
    * fix: closing

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,25 +1,24 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'QBX-Pawnshop'
+description 'QBX_Pawnshop'
 repository 'https://github.com/Qbox-project/qbx_pawnshop'
 version '1.0.0'
 
 shared_scripts {
-    '@qbx_core/import.lua',
+    '@ox_lib/init.lua',
     '@qbx_core/shared/locale.lua',
-    'config.lua',
     'locales/en.lua',
     'locales/*.lua',
-    '@ox_lib/init.lua'
-}
-
-server_scripts {
-    '@oxmysql/lib/MySQL.lua',
-    'server/main.lua'
+    'config.lua',
 }
 
 client_script 'client/main.lua'
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua',
+}
 
 lua54 'yes'
 use_experimental_fxv2_oal 'yes'
