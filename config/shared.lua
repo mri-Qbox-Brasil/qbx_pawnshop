79538f6a88601b56444eaa1ca794d79a27f12c30
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-07-11 15:34:28 -0300

    fix: coordenadas novas

--- config/shared.lua
+++ config/shared.lua
@@ -1,7 +1,7 @@
 return {
     pawnLocation = {
         [1] = {
-            coords = vector3(412.34, 314.81, 103.13),
+            coords = vec3(464.24, -770.7, 27.36),
             size = vector3(1.5, 1.8, 2.0),
             heading = 207.0,
             debugPoly = false,
